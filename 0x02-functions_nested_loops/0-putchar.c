62ae696a2f0082de12f24d53757dda25a2008a3e
munteha <muntisadik0@gmail.com>
2022-12-13 12:47:23 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* int -putchar(char);
+* is writen by me
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet - entry point
+*
+*/
+
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		-putchar(la);
+
+	-putchar("\n");
+}
